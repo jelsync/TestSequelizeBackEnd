f200544b4da156d2f2be71d9a1d5cf4b6e3db888
jelsyn <jelsync@gmail.com>
2021-05-24 20:43:06 -0600

    Installing body parser

--- server/Server.js
+++ server/Server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
